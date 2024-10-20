49ebe2b37864f71f7f1e002c7dbb5142001551ce
Ivan Mihun <vsk76090@gmail.com>
2024-10-20 18:13:05 +0200

    refactor: make encoding func return the corresponding huffman tree along with the encoded data

--- lib/huff.rb
+++ lib/huff.rb
@@ -17,19 +17,19 @@ module Huff
   end
 
   def encode(text)
-    huffman_tree = build_huffman_tree(create_occurrences_map(text))
+    huffman_tree = build_huffman_tree(text)
     codes = get_char_codes(huffman_tree)
-    text.each_char.map { |char| codes[char] }.join
+    { encoded_text: text.each_char.map { |char| codes[char] }.join, huffman_tree: huffman_tree }
   end
 
-  def decode(bits, tree)
+  def decode(bits, huffman_tree)
     text = ""
-    cur_node = tree
+    cur_node = huffman_tree
     bits.each_char do |bit|
       cur_node = bit == '0' ? cur_node.left : cur_node.right
       if cur_node.char
         text += cur_node.char
-        cur_node = tree
+        cur_node = huffman_tree
       end
     end
     text
@@ -43,7 +43,8 @@ module Huff
     occurrences_map
   end
 
-  def build_huffman_tree(occurrences_map)
+  def build_huffman_tree(text)
+    occurrences_map = create_occurrences_map(text)
     pq = PQueue.new { |a, b| a.frequency < b.frequency }
     occurrences_map.each { |char, count| pq.push(HuffNode.new(char, count)) }
     until pq.size == 1

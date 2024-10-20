bf47eefff221206f9150725a7a6534131e03ef79
Ivan Mihun <vsk76090@gmail.com>
2024-10-20 14:18:33 +0200

    feat: add method for creating occurrences map

--- lib/huff.rb
+++ lib/huff.rb
@@ -4,4 +4,12 @@ require_relative "huff/version"
 
 module Huff
   class Error < StandardError; end
+
+  private
+
+  def create_occurrences_map(text)
+    occurrences_map = Hash.new(0)
+    text.each_char { |char| occurrences_map[char] += 1 }
+    occurrences_map
+  end
 end
--- spec/huff_spec.rb
+++ spec/huff_spec.rb
@@ -1,11 +1,53 @@
 # frozen_string_literal: true
+require_relative '../lib/huff'
 
 RSpec.describe Huff do
   it "has a version number" do
     expect(Huff::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'create_occurrences_map' do
+    include Huff
+    let(:occurrences_map) { create_occurrences_map(text) }
+
+    context 'when the input string is empty' do
+      let(:text) { '' }
+
+      it 'returns an empty hash' do
+        expect(occurrences_map).to eq({})
+      end
+    end
+
+    context 'when the input string has unique characters' do
+      let(:text) { 'abc' }
+
+      it 'returns a hash with character counts of 1' do
+        expect(occurrences_map).to eq({ 'a' => 1, 'b' => 1, 'c' => 1 })
+      end
+    end
+
+    context 'when the input string has repeated characters' do
+      let(:text) { 'aabbcc' }
+
+      it 'returns a hash with correct character counts' do
+        expect(occurrences_map).to eq({ 'a' => 2, 'b' => 2, 'c' => 2 })
+      end
+    end
+
+    context 'when the input string has mixed characters' do
+      let(:text) { 'aabccdee' }
+
+      it 'returns a hash with correct character counts' do
+        expect(occurrences_map).to eq({ 'a' => 2, 'b' => 1, 'c' => 2, 'd' => 1, 'e' => 2 })
+      end
+    end
+
+    context 'when the input string contains special characters' do
+      let(:text) { '!!@@##$$' }
+
+      it 'returns a hash with correct counts for special characters' do
+        expect(occurrences_map).to eq({ '!' => 2, '@' => 2, '#' => 2, '$' => 2 })
+      end
+    end
   end
 end
